a87cbf3b18db19946ce4329a84fd847421d6af5d
Rinke <rinkehendriksen@gmail.com>
2019-06-24 13:54:59 +0200

    basic oauth2 implemented

--- chat/main.go
+++ chat/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,6 +10,11 @@ import (
 	"path/filepath"
 	"sync"
 	"trace"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/signature"
 )
 
 // templ represents a single template
@@ -25,13 +31,35 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, r)
 }
 
+func init() {
+	// load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No .env file found")
+	}
+}
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse() // parse the flags
+
+	// get github_secret and github_key
+	git_s, exists_s := os.LookupEnv("GITHUB_SECRET")
+
+	git_k, exists_k := os.LookupEnv("GITHUB_KEY")
+
+	if !exists_k || !exists_s {
+		log.Fatal("Error: github oauth2 credentials not given in .env file key: %s, secret: %s", exists_k, exists_s)
+	}
+	// setup gomniauth
+	gomniauth.SetSecurityKey(signature.RandomKey(64))
+	gomniauth.WithProviders(
+		github.New(git_k, git_s, "http://localhost:8080/auth/callback/github"),
+	)
+	fmt.Println(git_k, git_s)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// get the room going
 	go r.run()
